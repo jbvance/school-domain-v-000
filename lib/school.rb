b5c5ace12fbc8b6ed14c64eeca253f9ee984d78b
Jason Vance <jbvance@gmail.com>
2017-11-27 20:07:41 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,10 +1,18 @@
 class School
 
 attr_accessor :name, :roster
-  
+
   def initialize(name)
     @name = name
     @roster = {}
   end
-  
+
+  def add_student(name, grade)
+    if !@roster.key?(grade)
+      @roster[grade] = []
+    else
+      @roster[grade] << name
+    end
+  end
+
 end
